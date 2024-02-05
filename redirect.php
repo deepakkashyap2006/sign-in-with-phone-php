62f5ae1cb99ebe76d643afeb1d2c527aba055795
deepakkashyap2006 <157588018+deepakkashyap2006@users.noreply.github.com>
2024-02-05 14:15:43 +0530

    Update redirect.php

--- redirect.php
+++ redirect.php
@@ -7,7 +7,7 @@ use Firebase\JWT\Key;
 
 if(isset($_GET['phtoken'])){
     try {
-        $api_key = 'API_KEY'; // Please specify API key provided by Phone Email mobile application
+        $api_key = 'vndAsII3ioyVyyMZIOY6DrbsOBSaJdwF'; // Please specify API key provided by Phone Email mobile application
         $decoded = JWT::decode($_GET['phtoken'], new Key($api_key, 'HS256'));
         $jwt_response = 1;  // JWT decoded successfully
         $jwt_phone = $decoded->country_code.$decoded->phone_no; // You will get user phone number here from JWT
